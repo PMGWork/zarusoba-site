48027401fa3da5b834d1680d1b54ee6f113cd68b
ぴくせる <48606231+PMGWork@users.noreply.github.com>
2021-03-30 00:06:03 +0900

    update

---
+++ pages/moving01.vue
@@ -0,0 +1,104 @@
+<template>
+
+<div id="overflow">
+<div id="loading1" class="loading"></div>
+<div id="loading2" class="loading"></div>
+<div id="loading3" class="loading"></div>
+
+<header class="header">
+    <nuxt-link to="/"><img class="header-logo" src="~/assets/img/ZDW_logo.svg"></nuxt-link>
+    <ul>
+        <li>
+            <nuxt-link v-if="$device.isDesktop" v-scroll-to="'#top'" to>Top</nuxt-link>
+            <nuxt-link v-else v-scroll-to="{ el: '#top', container: '#scroll' }" to>Top</nuxt-link>
+        </li>
+    </ul>
+</header>
+
+<div id="luxy">
+    <div id="top" class="moving">
+        <h4 class="moving-title">M∞ving 01 音源ダウンロードについて</h4>
+        <div class="moving-content">
+            <p class="txt">M∞ving 01のご購入、ありがとうございます。</p>
+            <p class="txt">音源、および映像のダウンロードに関してですが、DLカードの有効期限切れてしまいましたので、現在ダウンロードができない状態にあります。</p>
+            <p class="txt">対策として、DLカードのシリアルコードをメールに添付の上、Zarusoba DesignWorksのメールアドレス宛に送付していただき、返信メールにて映像、音源データをお渡しする形をとらせていただきます。</p>
+            <p class="txt">お手数をおかけしてしまい、大変申し訳ございません。</p>
+            <div class="content-item">
+                <p class="subtitle font">Mail -</p>
+                <a href="mailto:zarusobadesignworks@gmail.com" target="_blank" aria-label="EMail" rel="noopener noreferrer">
+                    <h5 class="title">zarusobadesignworks@gmail.com</h5>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+import { gql } from 'graphql-request';
+
+export default {
+    async asyncData({ $graphcms }) {
+        const { edzs } = await $graphcms.request(
+            gql`
+                {
+                    edzs(orderBy: date_DESC) {
+                        title
+                        image {
+                            webp: url(
+                                transformation: {
+                                    document: { output: { format: webp } }
+                                }
+                            )
+                            url
+                            height
+                            width
+                        }
+                        link
+                        created
+                    }
+                }
+            `
+        );
+
+        return { edzs };
+    },
+    head() {
+        return {
+            title: 'Everyday Zarusoba | Zarusoba DesignWorks',
+        }
+    },
+    mounted(){
+        //Luxy
+        luxy.cancel();
+
+        var _ua = (function(u){
+            return {
+                Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1 && u.indexOf("tablet pc") == -1)
+                    || u.indexOf("ipad") != -1
+                    || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
+                    || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
+                    || u.indexOf("kindle") != -1
+                    || u.indexOf("silk") != -1
+                    || u.indexOf("playbook") != -1,
+                Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
+                    || u.indexOf("iphone") != -1
+                    || u.indexOf("ipod") != -1
+                    || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
+                    || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
+                    || u.indexOf("blackberry") != -1
+            }
+        })(window.navigator.userAgent.toLowerCase());
+
+        if(!_ua.Mobile&&!_ua.Tablet){
+            luxy.init();
+        } else {
+            luxy.init({
+                wrapperSpeed: 1
+            });
+        }
+    }
+}
+</script>
