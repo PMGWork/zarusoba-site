c653df43145bdabded071c36e0b92810f676f8de
ぴくせる <48606231+PMGWork@users.noreply.github.com>
2021-04-04 21:35:42 +0900

    update

--- pages/moving01.vue
+++ pages/moving01.vue
@@ -17,14 +17,14 @@
 
 <div id="luxy">
     <div id="top" class="moving">
-        <h4 class="moving-title">M∞ving 01 音源ダウンロードについて</h4>
+        <h4 class="moving-title">M∞ving 01 ダウンロードについて</h4>
         <div class="moving-content">
             <p class="txt font">M∞ving 01のご購入、ありがとうございます。</p>
             <p class="txt font">音源、および映像のダウンロードに関してですが、DLカードの有効期限切れてしまいましたので、現在ダウンロードができない状態にあります。</p>
-            <p class="txt font">対策として、DLカードのシリアルコードをメールに添付の上、Zarusoba DesignWorksのメールアドレス宛に送付していただき、返信メールにて映像、音源データをお渡しする形をとらせていただきます。</p>
+            <p class="txt font">対策として、DLカードのシリアルコードを下記のフォームに添付していただき、メール、またはTwitterのDMにて映像、音源データをお渡しする形をとらせていただきます。</p>
             <p class="txt font">お手数をおかけしてしまい、大変申し訳ございません。</p>
             <div class="content-item">
-                <p class="subtitle font">Mail -</p>
+                <p class="subtitle font">Form -</p>
                 <a href="https://forms.gle/E8dWvwsH2fWNbamT9" target="_blank" aria-label="form" rel="noopener noreferrer">
                     <h5 class="title">zarusobadesignworks@gmail.com</h5>
                 </a>
